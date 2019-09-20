6c30e1874cadabb1f72f37ce7550ec82bf8d8039
Ryan Hurey <RHUREY@MICROSOFT.COM>
2019-09-20 15:32:09 -0700

    Don't reuse variable names in telemetry verfication (#89)
    * Don't reuse variable names in telemetry verfication
    
    * And now hypothesis
    
    * When speech.end is detected, only shrink replay buffer for continuous recognition. Fix other bugs in telemetry verification code.
    
    * Fix whitespace

--- tests/TelemetryUtil.ts
+++ tests/TelemetryUtil.ts
@@ -4,19 +4,24 @@
 import { IMetric, ITelemetry } from "../src/common.speech/ServiceTelemetryListener.Internal";
 import { IStringDictionary } from "../src/common/IDictionary";
 
-export const validateTelemetry: (json: string, phrases: number, hypothesis: number) => void = (json: string, phrases: number, hypothesis: number): void => {
+export const validateTelemetry: (json: string, numPhrases: number, numHypothesis: number) => void = (json: string, numPhrases: number, numHypothesis: number): void => {
     const telemetryMessage: ITelemetry = JSON.parse(json);
 
-    if (0 > phrases) {
+    if (0 < numPhrases) {
         let phrases: string[] = telemetryMessage.ReceivedMessages["speech.phrase"];
         if (undefined === phrases) {
             phrases = telemetryMessage.ReceivedMessages["translation.phrase"];
         }
-        expect(phrases.length).toEqual(phrases);
+        expect(phrases).not.toBeUndefined();
+        expect(phrases.length).toEqual(numPhrases);
     }
-    if (0 > hypothesis) {
-        const hypothesis: string[] = telemetryMessage.ReceivedMessages["speech.hypothesis"];
-        expect(hypothesis.length).toEqual(hypothesis);
+
+    if (0 < numHypothesis) {
+        let hypothesis: string[] = telemetryMessage.ReceivedMessages["speech.hypothesis"];
+        if (undefined === hypothesis) {
+            hypothesis = telemetryMessage.ReceivedMessages["translation.hypothesis"];
+        }
+        expect(hypothesis.length).toEqual(numHypothesis);
     }
 
     let foundPhrases: number = 0;
@@ -24,12 +29,12 @@ export const validateTelemetry: (json: string, phrases: number, hypothesis: numb
         if (metric.PhraseLatencyMs !== undefined) {
             foundPhrases++;
             // For continuous recognition tests silence at the end my produce no last phrase.
-            expect(metric.PhraseLatencyMs.length).toBeLessThanOrEqual(phrases);
-            expect(metric.PhraseLatencyMs.length).toBeGreaterThanOrEqual(phrases - 1);
+            expect(metric.PhraseLatencyMs.length).toBeLessThanOrEqual(numPhrases);
+            expect(metric.PhraseLatencyMs.length).toBeGreaterThanOrEqual(numPhrases - 1);
         }
     }
 
-    expect(foundPhrases).toEqual(phrases === 0 ? 0 : 1);
+    expect(foundPhrases).toEqual(numPhrases === 0 ? 0 : 1);
 
     let foundHypothesis: number = 0;
     for (const metric of telemetryMessage.Metrics) {
@@ -37,10 +42,10 @@ export const validateTelemetry: (json: string, phrases: number, hypothesis: numb
             foundHypothesis++;
 
             // For continuous recognition tests silence at the end my produce no hypothesis.
-            expect(metric.FirstHypothesisLatencyMs.length).toBeLessThanOrEqual(phrases);
-            expect(metric.FirstHypothesisLatencyMs.length).toBeGreaterThanOrEqual(phrases - 1);
+            expect(metric.FirstHypothesisLatencyMs.length).toBeLessThanOrEqual(numPhrases);
+            expect(metric.FirstHypothesisLatencyMs.length).toBeGreaterThanOrEqual(numPhrases - 1);
         }
     }
 
-    expect(foundHypothesis).toEqual(phrases === 0 ? 0 : 1);
+    expect(foundHypothesis).toEqual(numPhrases === 0 ? 0 : 1);
 };
