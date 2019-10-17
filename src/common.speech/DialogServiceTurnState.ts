abc9b2730eab4d74c9d13f945d96349a31758b6e
Gabriel Ghizila <gghizila@microsoft.com>
2019-10-16 18:00:11 -0700

    Turn management classes

---
+++ src/common.speech/DialogServiceTurnState.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import { AudioOutputStream, PullAudioOutputStreamImpl } from "../sdk/Audio/AudioOutputStream";
+import { ActivityPayloadResponse, MessageDataStreamType } from "./ServiceMessages/ActivityResponsePayload";
+
+export class DialogServiceTurnState {
+    private privRequestId: string;
+    private privIsCompleted: boolean;
+    private privAudioStream: PullAudioOutputStreamImpl;
+
+    constructor(requestId: string) {
+        this.privRequestId = requestId;
+        this.privIsCompleted = false;
+        this.privAudioStream = null;
+    }
+
+    public get audioStream(): PullAudioOutputStreamImpl {
+        return this.privAudioStream;
+    }
+
+    public processActivityPayload(payload: ActivityPayloadResponse): PullAudioOutputStreamImpl {
+        if (payload.messageDataStreamType === MessageDataStreamType.TextToSpeechAudio) {
+            this.privAudioStream = AudioOutputStream.createPullStream() as PullAudioOutputStreamImpl;
+        }
+        return this.privAudioStream;
+    }
+
+    public complete(): void {
+        if (this.privAudioStream != null) {
+            this.privAudioStream.close();
+        }
+    }
+}
