87c2aaa5bc42f15db4c3863ce16e3cb052ca5bae
Gabriel Ghizila <gghizila@microsoft.com>
2019-10-17 15:06:07 -0700

    Added timeouts as workaround to not routed messages to 2s. Added debugturn logging for investingating the messages not routed. Added timestamps to console.info

--- src/common.speech/DialogServiceTurnState.ts
+++ src/common.speech/DialogServiceTurnState.ts
@@ -2,20 +2,29 @@
 // Licensed under the MIT license.
 
 import { AudioOutputStream, PullAudioOutputStreamImpl } from "../sdk/Audio/AudioOutputStream";
+import { DialogServiceTurnStateManager } from "./DialogServiceTurnStateManager";
 import { ActivityPayloadResponse, MessageDataStreamType } from "./ServiceMessages/ActivityResponsePayload";
 
 export class DialogServiceTurnState {
     private privRequestId: string;
     private privIsCompleted: boolean;
     private privAudioStream: PullAudioOutputStreamImpl;
+    private privTimeoutToken: any;
+    private privTurnManager: DialogServiceTurnStateManager;
 
-    constructor(requestId: string) {
+    constructor(manager: DialogServiceTurnStateManager, requestId: string) {
         this.privRequestId = requestId;
         this.privIsCompleted = false;
         this.privAudioStream = null;
+        this.privTurnManager = manager;
+        this.resetTurnEndTimeout();
+        // tslint:disable-next-line:no-console
+        console.info("DialogServiceTurnState debugturn start:" + this.privRequestId);
     }
 
     public get audioStream(): PullAudioOutputStreamImpl {
+        // Called when is needed to stream.
+        this.resetTurnEndTimeout();
         return this.privAudioStream;
     }
 
@@ -23,14 +32,34 @@ export class DialogServiceTurnState {
         if (payload.messageDataStreamType === MessageDataStreamType.TextToSpeechAudio) {
             this.privAudioStream = AudioOutputStream.createPullStream() as PullAudioOutputStreamImpl;
             // tslint:disable-next-line:no-console
-            console.info("Audio debugturn:" + this.privRequestId);
+            console.info("Audio start debugturn:" + this.privRequestId);
         }
         return this.privAudioStream;
     }
 
     public complete(): void {
-        if (this.privAudioStream != null) {
+        if (this.privTimeoutToken !== undefined) {
+            clearTimeout(this.privTimeoutToken);
+        }
+
+        if (this.privAudioStream !== null) {
             this.privAudioStream.close();
         }
+    }
+
+    private resetTurnEndTimeout(): void {
+        if (this.privTimeoutToken !== undefined) {
+            clearTimeout(this.privTimeoutToken);
+        }
+        // tslint:disable-next-line:no-console
+        console.info("Timeout reset debugturn:" + this.privRequestId);
+
+        this.privTimeoutToken = setTimeout((): void => {
+            // tslint:disable-next-line:no-console
+            console.info("Timeout complete debugturn:" + this.privRequestId);
+
+            this.privTurnManager.CompleteTurn(this.privRequestId);
+            return;
+        }, 2000);
     }
 }
