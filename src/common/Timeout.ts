361e1dd90341d5611d5966d41d70cded23bd86c6
Glenn Harper <glharper@microsoft.com>
2020-07-06 11:19:15 -0700

    Formatting, source header

--- src/common/Timeout.ts
+++ src/common/Timeout.ts
@@ -1,11 +1,10 @@
-
-import { createNoDashGuid } from "./Guid";
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 type TWorkerMessage = ICallNotification | IClearResponse | IErrorNotification | IErrorResponse;
 
 interface IClearResponse {
     error: null;
-
     id: number;
 }
 
@@ -13,9 +12,7 @@ interface IErrorNotification {
     error: {
         message: string;
     };
-
     id: null;
-
     result: null;
 }
 
@@ -23,17 +20,13 @@ interface IErrorResponse {
     error: {
         message: string;
     };
-
     id: number;
-
     result: null;
 }
 
 interface ICallNotification {
     id: null;
-
     method: "call";
-
     params: {
         timerId: number;
     };
@@ -41,23 +34,17 @@ interface ICallNotification {
 
 interface ISetNotification {
     id: null;
-
     method: "set";
-
     params: {
         delay: number;
-
         now: number;
-
         timerId: number;
     };
 }
 
 interface IClearRequest {
     id: number;
-
     method: "clear";
-
     params: {
         timerId: number;
     };
@@ -109,7 +96,7 @@ export class Timeout {
                 } else if (typeof idOrFunc !== "undefined") {
                     idOrFunc();
 
-                    // A timeout can be savely deleted because it is only called once.
+                    // A timeout can be safely deleted because it is only called once.
                     scheduledTimeoutFunctions.delete(timerId);
                 } else {
                     throw new Error("The timer is in an undefined state.");
@@ -184,7 +171,7 @@ export class Timeout {
 
             Timeout.workerTimers = Timeout.load(url);
 
-            // Bug #1: Edge doesn't like the URL to be revoked directly.
+            // Edge doesn't like the URL to be revoked directly.
             Timeout.workerTimers.setTimeout(() => URL.revokeObjectURL(url), 0);
 
             return Timeout.workerTimers as IWorkerTimers;
@@ -192,6 +179,7 @@ export class Timeout {
     }
 
     public static timers: () => IWorkerTimers = Timeout.loadWorkerTimers();
+
     private static isCallNotification = (message: TWorkerMessage): message is ICallNotification => {
         return (message as ICallNotification).method !== undefined && (message as ICallNotification).method === "call";
     }
@@ -199,5 +187,4 @@ export class Timeout {
     private static isClearResponse = (message: TWorkerMessage): message is IClearResponse => {
         return (message as IClearResponse).error === null && typeof message.id === "number";
     }
-
 }
