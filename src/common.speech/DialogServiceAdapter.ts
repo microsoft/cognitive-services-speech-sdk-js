068776211328dfa7f4d6bf9005e57661bfd50816
Brandon Miller <42854725+modnarb@users.noreply.github.com>
2020-07-14 11:24:10 -0700

    Pass the output format for audio results (#206)
    * set audio property for synth format
    
    * send tts over in config

--- src/common.speech/DialogServiceAdapter.ts
+++ src/common.speech/DialogServiceAdapter.ts
@@ -20,6 +20,7 @@ import {
     PromiseResult,
     ServiceEvent,
 } from "../common/Exports";
+import { AudioOutputFormatImpl } from "../sdk/Audio/AudioOutputFormat";
 import { PullAudioOutputStreamImpl } from "../sdk/Audio/AudioOutputStream";
 import { AudioStreamFormatImpl } from "../sdk/Audio/AudioStreamFormat";
 import {
@@ -37,6 +38,7 @@ import {
     SpeechRecognitionCanceledEventArgs,
     SpeechRecognitionEventArgs,
     SpeechRecognitionResult,
+    SpeechSynthesisOutputFormat,
 } from "../sdk/Exports";
 import { DialogServiceTurnStateManager } from "./DialogServiceTurnStateManager";
 import {
@@ -264,7 +266,7 @@ export class DialogServiceAdapter extends ServiceRecognizerBase {
                         this.agentConfig.set(updateAgentConfig);
                     }
 
-                    const pullAudioOutputStream: PullAudioOutputStreamImpl = turn.processActivityPayload(activityPayload);
+                    const pullAudioOutputStream: PullAudioOutputStreamImpl = turn.processActivityPayload(activityPayload, (SpeechSynthesisOutputFormat as any)[this.privDialogServiceConnector.properties.getProperty(PropertyId.SpeechServiceConnection_SynthOutputFormat, undefined)]);
                     const activity = new ActivityReceivedEventArgs(activityPayload.messagePayload, pullAudioOutputStream);
                     if (!!this.privDialogServiceConnector.activityReceived) {
                         try {
