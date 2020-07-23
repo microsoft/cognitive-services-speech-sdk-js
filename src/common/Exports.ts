cd44f89df40f5af7ce5ef211cb4130d8a0d172e7
glharper <64209257+glharper@users.noreply.github.com>
2020-07-23 08:27:20 -0700

    Use native event types for event management (#215)
    * Use native event types for event management
    
    * remove interface-name restriction
    
    * Better method typing

--- src/common/Exports.ts
+++ src/common/Exports.ts
@@ -5,6 +5,7 @@ export * from "./AudioSourceEvents";
 export * from "./ConnectionEvents";
 export * from "./ConnectionMessage";
 export * from "./ConnectionOpenResponse";
+export * from "./Emitter";
 export * from "./Error";
 export * from "./Events";
 export * from "./EventSource";
