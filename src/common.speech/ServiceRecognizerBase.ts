0a46b659674e985d65336032e6aceff06a56d292
Glenn Harper <64209257+glharper@users.noreply.github.com>
2021-02-17 10:26:08 -0800

    Correct microphone timing issue if mic stream ends occurs before stop… (#327)
    * Correct microphone timing issue if mic stream ends occurs before stopRecognizer.
    
    * Don't keep deviceType state in SRB

--- src/common.speech/ServiceRecognizerBase.ts
+++ src/common.speech/ServiceRecognizerBase.ts
@@ -37,6 +37,7 @@ import {
     RequestSession,
     SpeechContext,
     SpeechDetected,
+    type
 } from "./Exports";
 import {
     AuthInfo,
@@ -68,6 +69,7 @@ export abstract class ServiceRecognizerBase implements IDisposable {
     private privActivityTemplate: string;
     private privSetTimeout: (cb: () => void, delay: number) => number = setTimeout;
     private privAudioSource: IAudioSource;
+    private privIsLiveAudio: boolean = false;
     protected privSpeechContext: SpeechContext;
     protected privRequestSession: RequestSession;
     protected privConnectionId: string;
@@ -208,6 +210,7 @@ export abstract class ServiceRecognizerBase implements IDisposable {
         const audioStreamNode: IAudioStreamNode = await this.audioSource.attach(this.privRequestSession.audioNodeId);
         const format: AudioStreamFormatImpl = await this.audioSource.format;
         const deviceInfo: ISpeechConfigAudioDevice = await this.audioSource.deviceInfo;
+        this.privIsLiveAudio = deviceInfo.type && deviceInfo.type === type.Microphones;
 
         const audioNode = new ReplayableAudioNode(audioStreamNode, format.avgBytesPerSec);
         await this.privRequestSession.onAudioSourceAttachCompleted(audioNode, false);
@@ -692,7 +695,9 @@ export abstract class ServiceRecognizerBase implements IDisposable {
                     } else {
                         // the audio stream has been closed, no need to schedule next
                         // read-upload cycle.
-                        this.privRequestSession.onSpeechEnded();
+                        if (!this.privIsLiveAudio) {
+                            this.privRequestSession.onSpeechEnded();
+                        }
                     }
                 }
             }
