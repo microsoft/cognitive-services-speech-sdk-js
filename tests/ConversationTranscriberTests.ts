5a7ee3816acb6561d20045af0d2f761842c705c4
Glenn Harper <glharper@microsoft.com>
2021-03-24 09:58:08 -0700

    change test timeouts

--- tests/ConversationTranscriberTests.ts
+++ tests/ConversationTranscriberTests.ts
@@ -226,7 +226,7 @@ Settings.testIfDOMCondition("Create Conversation and add participants", (done: j
         (error: string) => {
             done.fail(error);
         });
-}, 500000);
+}, 50000);
 
 Settings.testIfDOMCondition("Leave Conversation", (done: jest.DoneCallback) => {
     // tslint:disable-next-line:no-console
@@ -387,8 +387,8 @@ Settings.testIfDOMCondition("Create Conversation and force disconnect", (done: j
     // tslint:disable-next-line:no-console
     console.info("Name: Create Conversation and force disconnect");
     const s: sdk.SpeechTranslationConfig = BuildSpeechConfig();
-    // Use 13s timeout since backend timeout is 10s.
-    s.setServiceProperty("maxConnectionDurationSecs", "13", sdk.ServicePropertyChannel.UriQueryParameter);
+    // Use 12s timeout since backend timeout is 10s.
+    s.setServiceProperty("maxConnectionDurationSecs", "12", sdk.ServicePropertyChannel.UriQueryParameter);
     objsToClose.push(s);
 
     const c: sdk.Conversation = CreateConversation(s);
@@ -443,4 +443,4 @@ Settings.testIfDOMCondition("Create Conversation and force disconnect", (done: j
         (error: string) => {
             done.fail(error);
         });
-}, 640000);
+}, 50000);
