e0a6ed354f0ceaf58ce919672c3cebf23b7af09a
Glenn Harper <glharper@microsoft.com>
2021-12-02 10:14:43 -0800

    add getWaveFormat API to AudioStreamFormat to support non-PCM wav formats, include test and test files

--- src/sdk/Audio/AudioStreamFormat.ts
+++ src/sdk/Audio/AudioStreamFormat.ts
@@ -31,6 +31,22 @@ export abstract class AudioStreamFormat {
     }
 
     /**
+     * Creates an audio stream format object with the specified format characteristics.
+     * @member AudioStreamFormat.getWaveFormat
+     * @function
+     * @public
+     * @param {number} samplesPerSecond - Sample rate, in samples per second (Hertz).
+     * @param {number} bitsPerSample - Bits per sample, typically 16.
+     * @param {number} channels - Number of channels in the waveform-audio data. Monaural data
+     *        uses one channel and stereo data uses two channels.
+     * @param {AudioFormatTag} format - Audio format (PCM, alaw or mulaw).
+     * @returns {AudioStreamFormat} The audio stream format being created.
+     */
+    public static getWaveFormat(samplesPerSecond: number, bitsPerSample: number, channels: number, format: AudioFormatTag): AudioStreamFormat {
+        return new AudioStreamFormatImpl(samplesPerSecond, bitsPerSample, channels, format);
+    }
+
+    /**
      * Creates an audio stream format object with the specified pcm waveformat characteristics.
      * @member AudioStreamFormat.getWaveFormatPCM
      * @function
@@ -72,6 +88,8 @@ export class AudioStreamFormatImpl extends AudioStreamFormat {
      */
     public constructor(samplesPerSec: number = 16000, bitsPerSample: number = 16, channels: number = 1, format: AudioFormatTag = AudioFormatTag.PCM) {
         super();
+
+        let isWavFormat: boolean = true;
         /* 1 for PCM; 6 for alaw; 7 for mulaw */
         switch (format) {
             case AudioFormatTag.PCM:
@@ -84,6 +102,7 @@ export class AudioStreamFormatImpl extends AudioStreamFormat {
                 this.formatTag = 7;
                 break;
             default:
+                isWavFormat = false;
         }
         this.bitsPerSample = bitsPerSample;
         this.samplesPerSec = samplesPerSec;
@@ -91,35 +110,37 @@ export class AudioStreamFormatImpl extends AudioStreamFormat {
         this.avgBytesPerSec = this.samplesPerSec * this.channels * (this.bitsPerSample / 8);
         this.blockAlign = this.channels * Math.max(this.bitsPerSample, 8);
 
-        this.privHeader = new ArrayBuffer(44);
+        if (isWavFormat) {
+            this.privHeader = new ArrayBuffer(44);
 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView
-        const view = new DataView(this.privHeader);
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView
+            const view = new DataView(this.privHeader);
 
-        /* RIFF identifier */
-        this.setString(view, 0, "RIFF");
-        /* file length */
-        view.setUint32(4, 0, true);
-        /* RIFF type & Format */
-        this.setString(view, 8, "WAVEfmt ");
-        /* format chunk length */
-        view.setUint32(16, 16, true);
-        /* audio format */
-        view.setUint16(20, this.formatTag, true);
-        /* channel count */
-        view.setUint16(22, this.channels, true);
-        /* sample rate */
-        view.setUint32(24, this.samplesPerSec, true);
-        /* byte rate (sample rate * block align) */
-        view.setUint32(28, this.avgBytesPerSec, true);
-        /* block align (channel count * bytes per sample) */
-        view.setUint16(32, this.channels * (this.bitsPerSample / 8), true);
-        /* bits per sample */
-        view.setUint16(34, this.bitsPerSample, true);
-        /* data chunk identifier */
-        this.setString(view, 36, "data");
-        /* data chunk length */
-        view.setUint32(40, 0, true);
+            /* RIFF identifier */
+            this.setString(view, 0, "RIFF");
+            /* file length */
+            view.setUint32(4, 0, true);
+            /* RIFF type & Format */
+            this.setString(view, 8, "WAVEfmt ");
+            /* format chunk length */
+            view.setUint32(16, 16, true);
+            /* audio format */
+            view.setUint16(20, this.formatTag, true);
+            /* channel count */
+            view.setUint16(22, this.channels, true);
+            /* sample rate */
+            view.setUint32(24, this.samplesPerSec, true);
+            /* byte rate (sample rate * block align) */
+            view.setUint32(28, this.avgBytesPerSec, true);
+            /* block align (channel count * bytes per sample) */
+            view.setUint16(32, this.channels * (this.bitsPerSample / 8), true);
+            /* bits per sample */
+            view.setUint16(34, this.bitsPerSample, true);
+            /* data chunk identifier */
+            this.setString(view, 36, "data");
+            /* data chunk length */
+            view.setUint32(40, 0, true);
+        }
     }
 
     /**
