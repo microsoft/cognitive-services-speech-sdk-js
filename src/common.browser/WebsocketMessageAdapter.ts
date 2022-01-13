ae38d763f2ef3506a22e9996a59af7bd11fd1ff8
Ryan Hurey <RHUREY@MICROSOFT.COM>
2022-01-12 17:50:25 -0800

    Add protocol to agent as workaround

--- src/common.browser/WebsocketMessageAdapter.ts
+++ src/common.browser/WebsocketMessageAdapter.ts
@@ -118,6 +118,11 @@ export class WebsocketMessageAdapter {
                 const checkAgent: CertCheckAgent = new CertCheckAgent(this.proxyInfo);
 
                 options.agent = checkAgent.GetAgent();
+
+                // Workaround for https://github.com/microsoft/cognitive-services-speech-sdk-js/issues/465
+                // Which is root caused by https://github.com/TooTallNate/node-agent-base/issues/61
+                const uri = new URL(this.privUri);
+                (options.agent as any).protocol = uri.protocol;
                 this.privWebsocketClient = new ws(this.privUri, options);
             }
 
