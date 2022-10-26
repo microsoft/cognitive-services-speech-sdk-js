1f34ac5a2607f7215fcf59c510377cf298f35d8f
Glenn Harper <64209257+glharper@users.noreply.github.com>
2022-10-26 16:08:52 -0700

    Use conventional idioms to access connection and phraseListGrammar fr… (#586)
    * Use conventional idioms to access connection and phraseListGrammar from ConversationTranscriber
    
    * set connection property on ConversationTranscriber as deprecated
    
    * add internalData property for ConversationTranscriber, remove recognizer property

--- src/sdk/PhraseListGrammar.ts
+++ src/sdk/PhraseListGrammar.ts
@@ -5,7 +5,10 @@ import {
     DynamicGrammarBuilder,
     ServiceRecognizerBase,
 } from "../common.speech/Exports";
-import { Recognizer } from "./Exports";
+import {
+    ConversationTranscriber,
+    Recognizer
+} from "./Exports";
 
 /**
  * Allows additions of new phrases to improve speech recognition.
@@ -24,9 +27,8 @@ export class PhraseListGrammar {
      * Creates a PhraseListGrammar from a given speech recognizer. Will accept any recognizer that derives from @class Recognizer.
      * @param recognizer The recognizer to add phrase lists to.
      */
-    public static fromRecognizer(recognizer: Recognizer): PhraseListGrammar {
-        const recoBase: ServiceRecognizerBase = recognizer.internalData as ServiceRecognizerBase;
-
+    public static fromRecognizer(recognizer: Recognizer | ConversationTranscriber): PhraseListGrammar {
+        const recoBase = recognizer.internalData as ServiceRecognizerBase;
         return new PhraseListGrammar(recoBase);
     }
 
