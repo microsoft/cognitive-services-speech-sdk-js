39d87f1400465ad2b14d3c4ed97cf5b578b53ae3
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2022-11-18 15:30:23 -0800

    Update LID properties (#592)
    Remove these (to align with the change in Carbon native):
    SpeechServiceConnection_AtStartLanguageIdPriority
    SpeechServiceConnection_ContinuousLanguageIdPriority
    
    Add this single one:
    SpeechServiceConnection_LanguageIdMode
    
    Change code such that LID configuration is driven by a single "mode". This is already exposed (before my change) as the AutoDetectSourceLanguageConfig.mode property, which can take two enum values LanguageIdMode.AtStart and LanguageIdMode.Continuous. They are reflected as a string in the single property SpeechServiceConnection_LanguageIdMode.
    
    We will only support "PrioritizeLatency" in the JS code. No support for "PrioritizeAccuracy".
    
    Update test to make sure speech.context is set properly when you set AutoDetectSourceLanguageConfig.mode to AtStart, Continuous or don't set it all, in which case it defaults to AtStart.
    
    I wrote sample code to test RecognizeOnce SR + AtStart LID, and StartContinuousRecognition SR + AtStart LID / Continuous LID. Everything works nicely. I tested with multilingual WAV file (multiple utterances, each one at different language). I check that v2 endpoint is automatically set for Continuous LID. I checked the values of speech.context and query URL. There is still work to be done to update the automated tests to do the same. I opened a task for that.

--- src/common.speech/RecognizerConfig.ts
+++ src/common.speech/RecognizerConfig.ts
@@ -19,7 +19,6 @@ export enum SpeechResultFormat {
 export class RecognizerConfig {
     private privRecognitionMode: RecognitionMode;
     private privLanguageIdMode: string;
-    private privLanguageIdPriority: string;
     private privSpeechServiceConfig: SpeechServiceConfig;
     private privRecognitionActivityTimeout: number;
     private privParameters: PropertyCollection;
@@ -31,11 +30,7 @@ export class RecognizerConfig {
         this.privSpeechServiceConfig = speechServiceConfig ? speechServiceConfig : new SpeechServiceConfig(new Context(null));
         this.privParameters = parameters;
         this.privMaxRetryCount = parseInt(parameters.getProperty("SPEECH-Error-MaxRetryCount", "4"), 10);
-        this.privLanguageIdPriority = parameters.getProperty(PropertyId.SpeechServiceConnection_ContinuousLanguageIdPriority, undefined);
-        this.privLanguageIdMode = this.privLanguageIdPriority === "Latency" ? "DetectContinuous" : "DetectAtAudioStart";
-        if (this.privLanguageIdMode === "DetectAtAudioStart") {
-            this.privLanguageIdPriority = parameters.getProperty(PropertyId.SpeechServiceConnection_AtStartLanguageIdPriority, undefined);
-        }
+        this.privLanguageIdMode = parameters.getProperty(PropertyId.SpeechServiceConnection_LanguageIdMode, undefined);
     }
 
     public get parameters(): PropertyCollection {
@@ -62,10 +57,6 @@ export class RecognizerConfig {
 
     public get isContinuousRecognition(): boolean {
         return this.privRecognitionMode !== RecognitionMode.Interactive;
-    }
-
-    public get languageIdPriority(): string {
-        return !!this.privLanguageIdPriority ? `Prioritize${this.privLanguageIdPriority}` : "";
     }
 
     public get languageIdMode(): string {
