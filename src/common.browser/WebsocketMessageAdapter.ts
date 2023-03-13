551336540c807d56bb964accd153a64b3d80a379
glharper@microsoft.com <glharper@microsoft.com>
2023-03-13 08:37:45 -0700

    remove ocsp code - test PR

--- src/common.browser/WebsocketMessageAdapter.ts
+++ src/common.browser/WebsocketMessageAdapter.ts
@@ -27,8 +27,6 @@ import {
 } from "../common/Exports";
 import { ProxyInfo } from "./ProxyInfo";
 
-import { CertCheckAgent } from "./CertChecks";
-
 interface ISendItem {
     Message: ConnectionMessage;
     RawWebsocketMessage: RawWebsocketMessage;
@@ -115,9 +113,6 @@ export class WebsocketMessageAdapter {
                 const options: ws.ClientOptions = { headers: this.privHeaders, perMessageDeflate: this.privEnableCompression };
                 // The ocsp library will handle validation for us and fail the connection if needed.
                 this.privCertificateValidatedDeferral.resolve();
-                const checkAgent: CertCheckAgent = new CertCheckAgent(this.proxyInfo);
-
-                options.agent = checkAgent.GetAgent();
 
                 // Workaround for https://github.com/microsoft/cognitive-services-speech-sdk-js/issues/465
                 // Which is root caused by https://github.com/TooTallNate/node-agent-base/issues/61
