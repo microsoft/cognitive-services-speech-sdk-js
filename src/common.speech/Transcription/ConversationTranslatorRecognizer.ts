d6f620965a50f59b9a8a87b7f3be96c56a6ecd5b
glharper@microsoft.com <glharper@microsoft.com>
2023-08-07 08:56:47 -0700

    correctly bind window.timeout

--- src/common.speech/Transcription/ConversationTranslatorRecognizer.ts
+++ src/common.speech/Transcription/ConversationTranslatorRecognizer.ts
@@ -79,8 +79,17 @@ export class ConversationTranslatorRecognizer extends Recognizer implements Conv
         this.privProperties = serviceConfigImpl.properties.clone();
         this.privConnection = Connection.fromRecognizer(this);
         const webWorkerLoadType: string = this.privProperties.getProperty(PropertyId.WebWorkerLoadType, "on").toLowerCase();
-        this.privSetTimeout = (webWorkerLoadType === "on" && typeof (Blob) !== "undefined" && typeof (Worker) !== "undefined") ? Timeout.setTimeout : setTimeout;
-        this.privClearTimeout = (webWorkerLoadType === "on" && typeof (Blob) !== "undefined" && typeof (Worker) !== "undefined") ? Timeout.clearTimeout : clearTimeout;
+        if (webWorkerLoadType === "on" && typeof (Blob) !== "undefined" && typeof (Worker) !== "undefined") {
+            this.privSetTimeout = Timeout.setTimeout;
+            this.privClearTimeout = Timeout.clearTimeout;
+        } else {
+            if (typeof window !== "undefined") {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                this.privSetTimeout = window.setTimeout.bind(window);
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                this.privClearTimeout = window.clearTimeout.bind(window);
+            }
+        }
     }
 
     public canceled: (sender: ConversationRecognizer, event: ConversationTranslationCanceledEventArgs) => void;
