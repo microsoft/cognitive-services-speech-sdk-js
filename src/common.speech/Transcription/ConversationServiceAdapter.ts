e0dc28f25d40b17c0aac06a390f58ea8421d2332
glharper@microsoft.com <glharper@microsoft.com>
2023-08-22 18:35:19 -0700

    fix bad changes

--- src/common.speech/Transcription/ConversationServiceAdapter.ts
+++ src/common.speech/Transcription/ConversationServiceAdapter.ts
@@ -412,7 +412,7 @@ export class ConversationServiceAdapter extends ServiceRecognizerBase {
                                         return Promise.resolve(authorizationToken);
                                     });
                                 this.authentication = token;
-                                await this.privConversationServiceConnector.onToken(token);
+                                this.privConversationServiceConnector.onToken(token);
 
                                 break;
 
