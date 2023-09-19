42fd7ddff2b18134ded2657e7d93ea6f7acb7aaa
glharper@microsoft.com <glharper@microsoft.com>
2023-09-19 06:45:56 -0700

    remove unneeded eslint ignore statements

--- src/common.browser/WebsocketMessageAdapter.ts
+++ src/common.browser/WebsocketMessageAdapter.ts
@@ -3,10 +3,6 @@
 
 // Node.JS specific web socket / browser support.
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as http from "http";
 import * as net from "net";
 import * as tls from "tls";
@@ -389,13 +385,11 @@ export class WebsocketMessageAdapter {
                 });
             });
         } else {
-            /* eslint-disable @typescript-eslint/no-unsafe-argument */
             if (!!options.secureEndpoint) {
                 socketPromise = Promise.resolve(tls.connect(options));
             } else {
                 socketPromise = Promise.resolve(net.connect(options));
             }
-            /* eslint-enable @typescript-eslint/no-unsafe-argument */
         }
 
         return socketPromise;
