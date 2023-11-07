d5f82b555a8681769fa4b31a0cf4b8057962ab05
glenn <glharper@microsoft.com>
2023-11-07 08:52:25 -0500

    Add note to dummy instantiation

--- microsoft.cognitiveservices.speech.sdk.ts
+++ microsoft.cognitiveservices.speech.sdk.ts
@@ -3,6 +3,9 @@
 // Licensed under the MIT license.
 import { AgentConfig } from "./src/common.speech/Exports.js";
 
+// Note: this instantiation ensures the above import isn't 
+// removed on compile. The import being absent causes an error on running
 void new AgentConfig();
+
 // Speech SDK API
 export * from "./src/sdk/Exports.js";
