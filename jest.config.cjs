723decc338fed181328f0a2f8873ddf1e0a260d6
Ryan Hurey <rhurey@microsoft.com>
2025-04-14 10:43:18 -0700

    Initial add of new config system.
    This moves the JS repo to use the same format JSON secrets configuration
    file as the natvie code base, and in CI/CD fetches the file from the KV
    location.
    
    Existing secrets management remains intact, so no dev inner loop need be
    changed.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
+const fs = require('fs');
+const path = require('path');
+
+// Default configuration file
+const defaultConfigFile = './secrets/TestConfiguration.ts';
+const configJsonFile = './secrets/test.subscriptions.regions.json';
+
+// Determine which configuration file to use
+const getConfigFile = () => {
+    if (configJsonFile && fs.existsSync(path.resolve(configJsonFile))) {
+        console.log(`Using JSON configuration: ${configJsonFile}`);
+        return configJsonFile;
+    }
+    console.log(`Using default configuration: ${defaultConfigFile}`);
+    return defaultConfigFile;
+};
+
+const configFile = getConfigFile();
+
 module.exports = {
     projects: [
         {
@@ -15,7 +34,7 @@ module.exports = {
             moduleFileExtensions: ["ts", "js", "jsx", "json", "node"],
             testEnvironment: "jsdom",
             collectCoverage: false,
-            setupFilesAfterEnv: ["./secrets/TestConfiguration.ts"],
+            setupFilesAfterEnv: [configFile],
             testTimeout : 20000
         },
         {
@@ -31,7 +50,7 @@ module.exports = {
             moduleFileExtensions: ["ts", "js", "jsx", "json", "node"],
             testEnvironment: "node",
             collectCoverage: false,
-            setupFilesAfterEnv: ["./secrets/TestConfiguration.ts"],
+            setupFilesAfterEnv: [configFile],
             testTimeout : 30000
         }
     ],
