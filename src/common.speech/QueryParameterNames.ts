848c63ab3fbab102f575fb4d81d99099629601d4
Emily Jiji <emilyjiji@microsoft.com>
2026-04-21 17:04:43 -0700

    remove dead constant

--- src/common.speech/QueryParameterNames.ts
+++ src/common.speech/QueryParameterNames.ts
@@ -20,7 +20,6 @@ export class QueryParameterNames {
     public static StableIntermediateThreshold: string = "stableIntermediateThreshold";
     public static StableTranslation: string = "stableTranslation";
     public static TestHooks: string = "testhooks";
-    public static Postprocessing: string = "postprocessing";
     public static CtsMeetingId: string = "meetingId";
     public static CtsDeviceId: string = "deviceId";
     public static CtsIsParticipant: string = "isParticipant";
